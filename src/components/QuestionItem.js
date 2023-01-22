a2cd031e78f7f642433d7a0a3aa3b39031000059
Preston <prestonosoro56@gmail.com>
2023-01-22 08:39:31 +0300

    call Answer and Delete Function to associate elements

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,16 +8,25 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
-
+  // configure handleDelete function to delete question
+  function handleDeleteClick(event) {
+    onDeleteClick(id);
+  }
+  // configure handleSubmitClick function to submit question
+  function handleAnswerClick(event) {
+    onAnswerChange(id, parseInt(event.target.value));
+  }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleAnswerClick}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
